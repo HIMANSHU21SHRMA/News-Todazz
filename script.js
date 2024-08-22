7440f29e538e7447fef445e5ddc3a9369c383596
himashu <himanshu12asdf@gmail.com>
2024-08-22 14:26:11 +0530

    nav bar and staacure ready!

---
+++ script.js
@@ -0,0 +1,6 @@
+// Your API key is: e651ee45eb1846b9a494206129c20f02
+
+
+// const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&sortBy=publishedAt&apiKey=${apiKey}`;
+    // 
+
