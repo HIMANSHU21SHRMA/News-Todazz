580df05987626a035ca6d2966ae099a158e673c8
himashu <himanshu12asdf@gmail.com>
2024-08-23 21:03:21 +0530

    apikey/ url collected

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// Your API key is: e651ee45eb1846b9a494206129c20f02
+apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}`
+const country = countrySelect.value;
+const language = languageSelect.value;
 
-
-// const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&sortBy=publishedAt&apiKey=${apiKey}`;
-    // 
-
-// const homePage = 
-const countrySelect = document.querySelector('#country');
-console.log(countrySelect.textContent);
- 
+const apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&language=${language}&apiKey=${apiKey}`;
