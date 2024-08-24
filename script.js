e2cfe8cb6e16dd5e0a28a88eb8aca91573e0169f
himashu <himanshu12asdf@gmail.com>
2024-08-24 20:16:42 +0530

    thing changes

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ const newCont = document.querySelector('#rightsec');
 const countrySelect = document.querySelector('#country')
 const languageSelect = document.querySelector('#lang')
 const date = new Date();
+console.log(date.toLocaleDateString());
+
+
 
 // funciton for creating and fetching
 
-const featchNews = async() => {
-       const country = countrySelect.value;
-       const language = languageSelect.value;
-       console.log(country, language);
+// const featchNews = async() => {
+     
+//        const country = countrySelect.value;
+//        const language = languageSelect.value;
+//        console.log(country, language);
        
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&language=${language}&apiKey=${apiKey}`;
+//     const apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&language=${language}&apiKey=${apiKey}`;
+
     
 
-}
-featchNews();
+// }
+
+// featchNews();
+// countrySelect.addEventListener('click',featchNews)
+// languageSelect.addEventListener('click',featchNews)
 
